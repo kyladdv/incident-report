38adf286d4e12f0564b936f397bfe20375133869
kyladdv <113135624+kyladdv@users.noreply.github.com>
2022-11-29 20:11:57 -0500

    Add files via upload

---
+++ Project-1/Group/routes/route.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express.app()
+
+const server= require('../gtoup/server')
+
+app.get('/', (req, res) => {}
+)
+
+app.get('/', incident.homeRoutes);
+
+app.get('/add-incident', views.add-incident)
+
+app.get('/update-incident', views.update-incident)
+
+app.post('/incident', controller.create);
+app.get('/incident', controller.find);
+app.put('/incident/:id', controller.update);
+app.delete('/incident/:id', controller.delete);
+
+module.exports = app
